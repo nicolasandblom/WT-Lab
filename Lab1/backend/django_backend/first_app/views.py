a8cb27c626ccc00df9ddf607e5dfe08b7ac43f8f
Nicola Sandblom <nicolalovisa@gmail.com>
2025-12-04 14:47:53 +0200

    more backend stuff

--- Lab1/backend/django_backend/first_app/views.py
+++ Lab1/backend/django_backend/first_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def students(request):
+    # define 3 students
+    # return the highest scorer among them
+    return render(request, 'students.html')
